cb0f9cb75c51eb83fb3538c4d7d8cd33d17c3f1b
Tbathurst715 <timbathurst@yahoo.com>
2022-05-15 20:46:33 -0600

    Created Variables

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,23 @@
-var now = moment ()
-var today = $('#currentDay')
+var Today = moment()
+$("#currentDay").text(Today.format('MMM Do YYYY, h:mm a'));
 
-today
+var timeBlock = $("#time-block")
+var row = $("#row")
+var hour = $("#hour")
+var past = $(".past")
+var present = $(".present")
+var future = $(".future")
+var saveBtn = $("#save-btn")
+var now = moment().hour.minute;
+
+hour = [
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16
+]; console.log(hour);
+
