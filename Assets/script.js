e01c504408fc11260457f9ac649cc4d0f4af939c
Tbathurst715 <timbathurst@yahoo.com>
2022-07-24 20:19:17 -0600

    updated textarea

--- Assets/script.js
+++ Assets/script.js
@@ -1,16 +1,16 @@
-var Today = moment()
+var Today = moment();
 $("#currentDay").text(Today.format('MMM Do YYYY, h:mm a'));
 
-var timeBlock = $("#time-block")
-var row = $("#row")
-var hour = $("#hour")
-var past = $(".past")
-var present = $(".present")
-var future = $(".future")
-var saveBtn = $("#save-btn")
-var now = moment().hour.minute;
+var timeBlock = $("#time-block");
+var row = $("#row");
+var hour = $("#hour");
+var past = $(".past");
+var present = $(".present");
+var future = $(".future");
+var saveBtn = $("#save-btn");
+var now = moment().hour;
 
-hour = [
+var hour = [
   9,
   10,
   11,
@@ -20,4 +20,30 @@ hour = [
   15,
   16
 ]; console.log(hour);
+
+function dayPlan() {
+  
+  var hourSet = parseInt($(this).attr('id').split('hourSet')[1]);
+  console.log(now, hourSet);
+
+  if (now > hourSet) {
+    $(this).addClass('future');
+    $(this).removeClass('past');
+    $(this).removeClass('present');
+  }
+    
+  else if (now < hourSet) {
+    $(this).addClass('past');
+    $(this).removeClass('future');
+    $(this).removeClass('present');
+  }
+
+  else {
+    $(this).addClass('present');
+    $(this).removeClass('future');
+    $(this).removeClass('past');
+  }
+
+
+};
 
